e1f77ee411d66c46e8d3ffa679930d12670f5686
paelomoldes <scdymlqgivckpdz66md3sstff4@gmail.com>
2026-06-05 22:00:47 +0800

    sync

--- functions/api.js
+++ functions/api.js
@@ -1,1 +1,18 @@
-throw new Error("error");
+
+function authentication(context) {
+  if (context.request.headers.get("x-api-key") != context.env.API_KEY) {
+    return new Response("Unauthorized", { status: 403 });
+  }
+
+  return context.next();
+}
+
+async function post(context) {
+  return new Response("success")
+}
+
+export async function onRequestGet(context) {
+  return new Response("data");
+}
+
+export const onRequestPost = [authentication, post];
