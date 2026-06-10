1f3cf8c9f7037f394da950bd52e2edc00180df0c
paelomoldes <scdymlqgivckpdz66md3sstff4@gmail.com>
2026-06-10 13:02:02 +0800

    sync

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,7 +3,7 @@ export async function onRequest({ env, next, request }) {
     return await next();
   } catch (e) {
     const status = 500;
-    if (request.headers.get("x-api-key") == env.API_KEY) return new Response(e.message, { status });
+    if (request.headers.get("x-api-key") == env.API_KEY) return new Response(`${e.message}\n${e.stack}`, { status });
     else return new Response(null, { status });
   }
 }
