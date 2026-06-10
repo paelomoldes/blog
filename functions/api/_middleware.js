d233133048e46838d72d1a3ae363538f67638a80
paelomoldes <scdymlqgivckpdz66md3sstff4@gmail.com>
2026-06-11 02:59:28 +0800

    sync

--- functions/api/_middleware.js
+++ functions/api/_middleware.js
@@ -1,4 +1,4 @@
 export function onRequest({ request, next }) {
-  if (request.headers.get('x-csrf-token') != request.headers.get("cookie").split('; ').find((row) => row.startsWith('__Secure-csrf-token='))?.split('=')[1]) return new Response(null, { status: 403 });
+  if (request.headers.get('x-csrf-token') ?? '' != request.headers.get("cookie").split('; ').find((row) => row.startsWith('csrf-token='))?.split('=')[1]) return new Response(null, { status: 403 });
   return next();
 }
