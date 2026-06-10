deb77754b85540f6e198c68d679afa28544e853f
paelomoldes <scdymlqgivckpdz66md3sstff4@gmail.com>
2026-06-10 14:20:34 +0800

    sync

--- functions/api/posts/index.js
+++ functions/api/posts/index.js
@@ -1,9 +1,9 @@
 const NS = 'post';
 
 export async function onRequestGet({ env }) {
-  return Response.json(await env.KV.list({ prefix: `${env.KV_PUBLIC}:${NS}:` }));
+  return Response.json(await env.KV.list({ prefix: `public:${NS}:` }));
 }
 
 export async function onRequestPost({ env, request }) {
-  return Response.json(await env.KV.list({ ...await request.json(), prefix: `${env.KV_PUBLIC}:${NS}:` }));
+  return Response.json(await env.KV.list({ ...await request.json(), prefix: `public:${NS}:` }));
 }
