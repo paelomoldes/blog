c732316eb85b8020e2714bdc6feba5af2b644e11
paelomoldes <scdymlqgivckpdz66md3sstff4@gmail.com>
2026-06-11 14:09:47 +0800

    sync

--- functions/api/_middleware.js
+++ functions/api/_middleware.js
@@ -1,4 +1,6 @@
 export function onRequest({ request, next }) {
-  if (request.headers.get('x-csrf-token') ?? '' != request.headers.get("cookie").split('; ').find((row) => row.startsWith('csrf-token='))?.split('=')[1]) return new Response(null, { status: 403 });
-  return next();
+  const tokenX = request.headers.get('x-csrf-token'),
+        tokenY = request.headers.get('cookie').split('; ').find((row) => row.startsWith('csrf-token='))?.split('=')[1];
+  if (tokenX && tokenY && tokenX == tokenY) return next();
+  return new Response(null, { status: 403 });
 }
