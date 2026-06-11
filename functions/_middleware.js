7d05a070225f311a8689b795d0d1306427608346
paelomoldes <scdymlqgivckpdz66md3sstff4@gmail.com>
2026-06-11 16:30:50 +0800

    sync

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -9,7 +9,8 @@ export const onRequest = [
   async ({ env, request, next }) => {
     const response = (await next()).clone();
     if (request.url.startsWith('https:')) response.headers.set('strict-transport-security', 'max-age=31536000');
-    response.headers.set('content-security-policy', env.CSP);
+    response.headers.set('content-security-policy', env.ContentSecurityPolicy);
+    response.headers.set('permissions-policy', env.PermissionsPolicy);
     response.headers.set('set-cookie', `csrf-token=${ crypto.randomUUID() }; Domain=paelo.pages.dev; Path=/; SameSite=Strict`);
     return response;
   }
