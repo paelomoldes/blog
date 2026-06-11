88cd8517d46ecc4474077db5ca06cddcf4ad561d
paelomoldes <scdymlqgivckpdz66md3sstff4@gmail.com>
2026-06-11 17:57:39 +0800

    sync

---
+++ functions/assets/_middleware.js
@@ -0,0 +1,5 @@
+export function onRequestGet({ next }) {
+  const response = (await next()).clone();
+  response.headers.set('cache-control', 'public, max-age=31536000, immutable');
+  return response;
+}
