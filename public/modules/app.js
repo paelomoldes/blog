207fce83c7dfd0c2476751580d341167a0376dd7
paelomoldes <scdymlqgivckpdz66md3sstff4@gmail.com>
2026-06-15 03:11:26 +0800

    sync

--- public/modules/app.js
+++ public/modules/app.js
@@ -1,51 +1,55 @@
-import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+// import config from './modules/config.json' with { type: 'json' }
+const config = await (await fetch('./modules/config.json')).json()
 
 
-const CONFIG = await (await fetch('./modules/config.json')).json(), /* import config from './modules/config.json' with { type: 'json' } */
-      INDEX = 'index',
-      INDEX_FALLBACK_SIGNATURE = '<!--@-->';
+const INDEX = 'index',
+      INDEX_FALLBACK_SIGNATURE = '<!--@-->'
 
 
 async function load(templateId) {
-  let template = '', functions = {};
-
-  templateId = templateId.replaceAll('/', '').trim();
+  let template = '', functions = {}
 
   try {
-    const moduleName = `${ location.origin }/modules/theme/${ templateId }.html`;
-    const response = await fetch(moduleName), error = new TypeError(`Failed to fetch dynamically imported module: ${ moduleName }`);
-    if (response.status != 200) throw error;
-    const body = await response.text();
-    if (body.startsWith(INDEX_FALLBACK_SIGNATURE)) throw error;
-    template = body;
+    const moduleName = `${ location.origin }/modules/theme/${ templateId }.html`
+    const response = await fetch(moduleName), error = new TypeError(`Failed to fetch dynamically imported module: ${ moduleName }`)
+    if (response.status != 200) throw error
+    const body = await response.text()
+    if (body.startsWith(INDEX_FALLBACK_SIGNATURE)) throw error
+    template = body
   } catch (e) {
-    console.error(e);
+    console.error(e)
   }
 
   {
-    const moduleName = `${ location.origin }/modules/theme/${ templateId }.js`;
+    const moduleName = `${ location.origin }/modules/theme/${ templateId }.js`
     try {
-      functions = await import(moduleName);
+      functions = await import(moduleName)
     } catch (e) {
-      console.error(new TypeError(`Failed to fetch dynamically imported module: ${ moduleName }`));
+      console.error(new TypeError(`Failed to fetch dynamically imported module: ${ moduleName }`))
     }
   }
 
-  return { template, ...functions };
+  return { template, ...functions }
 }
 
 
-const routes = [];
+const routes = []
 
-for (const templateId in CONFIG.routes) {
-  const path = CONFIG.routes[templateId], component = await load(templateId);
+for (let templateId in config.routes) {
+
+  templateId = templateId.replaceAll('/', '').trim()
+
+  if (templateId === INDEX) continue
+
+  const path = config.routes[templateId], component = await load(templateId)
 
   routes.push({ path, component })
 }
 
 
-const app = createApp(await load('index'));
+const app = createApp(await load(INDEX))
 
-app.use(createRouter({ history: createWebHistory(), routes }));
-app.mount(document.body);
+app.use(createRouter({ history: createWebHistory(), routes }))
+app.mount(document.body)
